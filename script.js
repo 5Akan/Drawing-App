352010dac499f641740f3927ca05d93610d3ff82
akanimoh <oakanimoh@gmail.com>
2022-10-04 15:57:45 +0100

    Updated files

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const canvas = document.getElementById('canvas');
 
 const ctx = canvas.getContext('2d'); 
+/*Gets the context , the context is
+ used to draw on the canvas Element*/
 
-let size = 10;
+let size = 30;
+let x = 50;
+let y = 50;
 
 canvas.addEventListener('mousedown', ()=>{
 
@@ -10,19 +14,30 @@ canvas.addEventListener('mousedown', ()=>{
 
 function drawCircle(x, y) {
     ctx.beginPath();
-    ctx.arc(x, y, size, 0, Math.PI * 2);//Creates an arc
+
     /*
     ctx.arc(x,y,radius,startradius,endradius,clockwise)
     */
-   ctx.fill();//This method is used to actually draw the arc
+    ctx.arc(x, y, size, 0, Math.PI * 2);//Creates an arc
+    
+   ctx.fill();//This method is used to actually draw(stroke) or fill(fill) the arc
 }
 
 
 
 function draw(params) {
-    drawCircle(50,50);
+    /*Method to clear out a specified rectangular 
+    size of the canvas*/
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    requestAnimationFrame(draw);//Watch video on it
+    drawCircle(x ,y);
+
+    /*Tells the browser that you wish to perform an
+    animation and requests the browser calls out the 
+    specified function to update an animation anytime the
+    broswer repaints
+    */
+    requestAnimationFrame(draw);
 }
 
 draw();
