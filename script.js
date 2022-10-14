3c64c60aa7ed8b10bfcbce3c05509d91b13073f2
akanimoh <oakanimoh@gmail.com>
2022-10-14 15:47:48 +0100

    Updated existing and also created new functions and event listeners

--- script.js
+++ script.js
@@ -11,30 +11,8 @@ const ctx = canvas.getContext('2d');
 let size = 10;
 let isPressed = false;
 let color = 'black';
-
-canvas.addEventListener('mousedown', (e)=>{
-    isPressed = true;
-});
-
-canvas.addEventListener('mouseup', (e)=>{
-    isPressed = false;
-});
-
-canvas.addEventListener('mousemove', (e)=>{
-    if (isPressed == true) {
-        /*Returns the x-coordinate of the mouse pointer ie
-     left and right of the target element in this case
-      the canvas      
-    */
-
-    const x = e.offsetX; 
-    const y = e.offsetY;// Above applies for offsetY
-    /*Together both offsetX and offsetY return an exact
-     point when the mouse is clicked */
-
-    drawCircle(x, y);
-    }
-});
+let x = undefined;
+let y = undefined;
 
 incrBtn.addEventListener('click', ()=>{
     size +=5;
@@ -56,6 +34,42 @@ decrBtn.addEventListener('click', ()=>{
     sizeUpdateOnScreen();
 });
 
+canvas.addEventListener('mousedown', (e)=>{
+    isPressed = true;
+
+    x = e.offsetX;
+    y = e.offsetY;
+});
+
+canvas.addEventListener('mouseup', (e)=>{
+    isPressed = false;
+
+    //So that when mouse is released destination point ceases there
+    x = undefined;
+    y = undefined;
+});
+
+canvas.addEventListener('mousemove', (e)=>{
+    if (isPressed == true) {
+     /*Returns the x-coordinate of the mouse pointer ie
+        left and right of the target element in this case
+        the canvas      
+    */
+
+    const x2 = e.offsetX; 
+    const y2 = e.offsetY;// Above applies for offsetY
+    /*Together both offsetX and offsetY return an exact
+     point when the mouse is clicked */
+
+    drawLine(x, y, x2, y2);
+    x = x2;//Because the destination point(x) is anywhere our mouse pointer stops
+    y = y2;
+
+    }
+});
+
+// drawLine(300, 300, 500, 500);
+
 colorEl.addEventListener('change', (e)=>{
     color = e.target.value;
 });
@@ -70,6 +84,18 @@ function drawCircle(x, y) {
     ctx.fillStyle = color;//For colours
     
    ctx.fill();//This method is used to actually draw(stroke) or fill(fill) the arc
+}
+
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath(); 
+    //A destination point where the line ends
+    ctx.moveTo(x1, y1);
+    
+    //Creates another point and draws a line from that point to the destination point
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+    
 }
 
 function sizeUpdateOnScreen(params) {
